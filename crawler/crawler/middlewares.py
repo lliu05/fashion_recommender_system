8221d7b36a48e37a2abde160308521fb2bf5ceb0
mxlei01 <mxlei01@gmail.com>
2017-09-07 20:01:47 -0600

    Fixed ascii issue.

--- crawler/crawler/middlewares.py
+++ crawler/crawler/middlewares.py
@@ -1,7 +1,7 @@
 """middlewares.py contains Scrapy middlewares.
 
 The spider middleware is a framework of hooks into Scrapy’s spider processing mechanism where you can plug custom
-'functionality to process the responses that are sent to Spiders for processing and to process the requests and items
+functionality to process the responses that are sent to Spiders for processing and to process the requests and items
 that are generated from spiders.
 
 More Info:
